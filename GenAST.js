8a6da9d14e8a236f6cd3479f25c009f3663eb4db
sam bacha <sam@freighttrust.com>
2021-04-10 22:14:24 -0700

    Update GenAST.js

--- GenAST.js
+++ GenAST.js
@@ -1,10 +1,10 @@
 "use strict";
 
-var thrift = require("thrift");
-var Thrift = thrift.Thrift;
-var Q = thrift.Q;
+const thrift = require("thrift");
+const Thrift = thrift.Thrift;
+const Q = thrift.Q;
 
-var ttypes = require("./GenAST_types");
+const ttypes = require("./GenAST_types");
 
 var GenAST_genAST_args = function (args) {
 	this.sol = null;
